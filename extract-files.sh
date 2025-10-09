09ccc524770f8a0cf865e7f7867b985009d69284
anrui2032 <anrui2032@gmail.com>
2025-10-09 11:22:53 +0800

    tenet: Import camera stack from GEN_ZTE_P768A02V1.0.1B04
    Change-Id: I0dd198c6235ad0031c215bc6a718028586bae562

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            "${SIGSCAN}" -p "13 0a 00 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.bitra.so)
             "${SIGSCAN}" -p "13 0a 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
